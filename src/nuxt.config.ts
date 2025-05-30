4afaf26ad3a48e4b021645d8ec61a74baeb8d2f1
sogo.komatsuzaki.xx <sogo.komatsuzaki.xx@ambl.co.jp>
2025-05-30 17:59:17 +0900

    feat 日付修正

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -8,7 +8,7 @@ import {
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-05-30',
   devtools: { enabled: true },
   css: ['@/styles/main.scss'],
   hooks: {
